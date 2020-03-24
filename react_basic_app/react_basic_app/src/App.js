6c16d6a215c02b337a3358682794c31ad1beb0fb
Rob Armitage <robert.c.armitage@gmail.com>
2020-03-24 23:36:46 +0000

    add Main.js which will be the homepage containing the Test Page link

--- react_basic_app/react_basic_app/src/App.js
+++ react_basic_app/react_basic_app/src/App.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Link} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is an app. Rob is a master hacker.
         </p>
         <p>
           <Link to="../components/TestPage.js">Test Page</Link>
         </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
