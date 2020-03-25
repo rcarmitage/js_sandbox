1bbdc1ffb1b7b322e284a9b9832e39eec550ffb2
Rob Armitage <robert.c.armitage@gmail.com>
2020-03-25 00:01:07 +0000

    add Navbar links, haven't checked

--- react_basic_app/react_basic_app/src/components/NavBar.js
+++ react_basic_app/react_basic_app/src/components/NavBar.js
@@ -3,6 +3,13 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     <ul>
-        
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="test-page">Test Page</Link>
+        </li>
     </ul>
-}
+};
+
+export default NavBar;
