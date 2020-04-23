37055a0a5b72a48f0b881d92d7486ea9e99964e2
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-23 22:25:45 +0100

    deleted import DataItem in DataComponent because it wasn't needed

--- react_events_app/events_v1/src/components/DataRender/DataComponent.js
+++ react_events_app/events_v1/src/components/DataRender/DataComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import DataList from './DataList';
-import DataItem from './DataItem';
 
 class DataComponent extends Component {
   constructor(props) {
