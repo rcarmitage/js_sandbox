7037cce7075ed017cc078c5d9ec6622005f5c3a8
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-23 14:56:42 +0100

    finished adding routes to react_events_app

---
+++ react_events_app/events_v1/src/components/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ErrorPage = () => (
+  <h4>404 - PAGE NOT FOUND</h4>
+)
+
+export default ErrorPage;
--- react_events_app/events_v1/src/components/Main.js
+++ react_events_app/events_v1/src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
 import Home from '../components/Home';
+import DataRenderPage from '../components/DataRenderPage';
+import ErrorPage from './ErrorPage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class Main extends Component {
@@ -11,6 +13,8 @@ class Main extends Component {
           <NavBar />
           <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/data-render-page" component={DataRenderPage} />
+          <Route component={ErrorPage} />
           </Switch>
         </React.Fragment>
       </Router>
